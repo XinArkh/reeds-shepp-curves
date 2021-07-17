a25a1573d95ac228fd6cbc34971904417687fac4
Wu Xin <wuhsin@zju.edu.cn>
2021-07-17 12:38:55 +0800

    utils and reeds_shepp rewritten

---
+++ reeds_shepp.py
@@ -0,0 +1,411 @@
+import math
+from enum import Enum
+import utils
+
+
+class Steering(Enum):
+    LEFT = 1
+    STRAIGHT = 0
+    RIGHT = -1
+
+
+class Gear(Enum):
+    FORWARD = 1
+    BACKWORD = -1
+
+
+class WordElement():
+    def __init__(self, param, steering, gear):
+        self.param = param
+        self.steering = steering
+        self.gear = gear
+
+    def __repr__(self):
+        if self.steering == Steering.LEFT:
+            steering_str = 'left'
+        elif self.steering == Steering.RIGHT:
+            steering_str = 'right'
+        else:
+            steering_str = 'straight'
+
+        if self.gear == Gear.FORWARD:
+            gear_str = 'forward'
+        else:
+            gear_str = 'backward'
+
+        info_str = '{ Steering: ' + steering_str + '\tGear: ' + gear_str \
+                   + '\tLength: ' + str(round(self.param, 2)) + '}'
+
+        return info_str
+
+    def reverse_steering(self):
+        if self.steering == Steering.LEFT:
+            self.steering = Steering.RIGHT
+        elif self.steering == Steering.RIGHT:
+            self.steering = Steering.LEFT
+
+    def reverse_gear(self):
+        self.gear = Gear.BACKWORD if self.gear == Gear.FORWARD else Gear.FORWARD
+
+
+def word_length(word):
+    return sum([abs(w_elem.param) for w_elem in word])
+
+
+def timeflip(word):
+    word_new = word.copy()
+    for w_elem in word_new:
+        w_elem.reverse_gear()
+    return word_new
+
+
+def reflect(word):
+    word_new = word.copy()
+    for w_elem in word_new:
+        w_elem.reverse_steering()
+    return word_new
+
+
+def get_all_words(p1, p2):
+    formulas = [word_cluster_1, word_cluster_2, word_cluster_3, 
+                word_cluster_4, word_cluster_5, word_cluster_6, 
+                word_cluster_7, word_cluster_8, word_cluster_9, 
+                word_cluster_10, word_cluster_11, word_cluster_12]
+    x, y, phi = utils.normalize_basis(p1, p2)
+    words = []
+    for f in formulas:
+        words.append(f(x, y, phi))
+        words.append(timeflip(f(-x, y, -phi)))
+        words.append(reflect(f(x, -y, -phi)))
+        words.append(reflect(timeflip(f(-x, -y, phi))))
+
+    # remove word elements that have parameter 0
+    for i in range(len(words)):
+        words[i] = list(filter(lambda e: e.param != 0, words[i]))
+
+    # remove empty words
+    words = list(filter(None, words))
+
+    return words
+
+
+def get_optimal_word(p1 ,p2):
+    words = get_all_words(p1, p2)
+
+    i_min = -1
+    L_min = float('inf')
+
+    for i, word in enumerate(words):
+        L = word_length(word)
+        if L <= L_min:
+            i_min, L_min = i, L
+
+    return words[i_min]
+
+
+def word_cluster_1(x, y, phi):
+    """
+    Formula 8.1 for CSC words
+    """
+    word = []
+
+    u, t = utils.R(x - math.sin(phi), y - 1 + math.cos(phi))
+    v = utils.M(phi - t)
+
+    params = [t, u, v]
+    steerings = [Steering.LEFT, Steering.STRAIGHT, Steering.LEFT]
+    gears = [Gear.FORWARD, Gear.FORWARD, Gear.FORWARD]
+
+    for i in range(len(params)):
+        word.append(WordElement(params[i], steerings[i], gears[i]))
+
+    return word
+
+
+def word_cluster_2(x, y, phi):
+    """
+    Formula 8.2 for CSC words
+    """
+    word = []
+
+    u1, t1 = utils.R(x + math.sin(phi), y - 1 - math.cos(phi))
+    
+    if u1**2 >= 4:
+        u = math.sqrt(u1**2 - 4)
+        _, theta = utils.R(u, 2)
+        t = utils.M(t1 + theta)
+        v = utils.M(t - phi)
+
+        params = [t, u, v]
+        steerings = [Steering.LEFT, Steering.STRAIGHT, Steering.RIGHT]
+        gears = [Gear.FORWARD, Gear.FORWARD, Gear.FORWARD]
+
+        for i in range(len(params)):
+            word.append(WordElement(params[i], steerings[i], gears[i]))
+
+    return word
+
+
+def word_cluster_3(x, y, phi):
+    """
+    Formule 8.3 for C|C|C words    *** TYPO IN PAPER ***
+    """
+    word = []
+
+    xi = x - math.sin(phi)
+    eta = y - 1 + math.cos(phi)
+    rho, theta = utils.R(xi, eta)
+
+    if rho <= 4:
+        A = math.acos(rho / 4)
+        t = utils.M(theta + math.pi/2 + A)
+        u = utils.M(math.pi - 2*A)
+        v = utils.M(phi - t - u)
+
+        params = [t, u, v]
+        steerings = [Steering.LEFT, Steering.RIGHT, Steering.LEFT]
+        gears = [Gear.FORWARD, Gear.BACKWORD, Gear.FORWARD]
+
+        for i in range(len(params)):
+            word.append(WordElement(params[i], steerings[i], gears[i]))
+
+    return word
+
+
+def word_cluster_4(x, y, phi):
+    """
+    Formule 8.4 for C|CC words    *** TYPO IN PAPER ***
+    """
+    word = []
+
+    xi = x - math.sin(phi)
+    eta = y - 1 + math.cos(phi)
+    rho, theta = utils.R(xi, eta)
+
+    if rho <= 4:
+        A = math.acos(rho / 4)
+        t = utils.M(theta + math.pi/2 + A)
+        u = utils.M(math.pi - 2*A)
+        v = utils.M(t + u - phi)
+
+        params = [t, u, v]
+        steerings = [Steering.LEFT, Steering.RIGHT, Steering.LEFT]
+        gears = [Gear.FORWARD, Gear.BACKWORD, Gear.BACKWORD]
+
+        for i in range(len(params)):
+            word.append(WordElement(params[i], steerings[i], gears[i]))
+
+    return word
+
+
+def word_cluster_5(x, y, phi):
+    """
+    Formule 8.4 for CC|C words    *** TYPO IN PAPER ***
+    """
+    word = []
+
+    xi = x - math.sin(phi)
+    eta = y - 1 + math.cos(phi)
+    rho, theta = utils.R(xi, eta)
+
+    if rho <= 4:
+        u = math.acos(1 - rho**2 / 8)
+        A = math.asin(2 * math.sin(u) / rho)
+        t = utils.M(theta + math.pi/2 - A)
+        v = utils.M(t - u - phi)
+
+        params = [t, u, v]
+        steerings = [Steering.LEFT, Steering.RIGHT, Steering.LEFT]
+        gears = [Gear.FORWARD, Gear.FORWARD, Gear.BACKWORD]
+
+        for i in range(len(params)):
+            word.append(WordElement(params[i], steerings[i], gears[i]))
+
+    return word
+
+
+def word_cluster_6(x, y, phi):
+    """
+    Formule 8.7 for CCu|CuC words
+    """
+    word = []
+
+    xi = x + math.sin(phi)
+    eta = y - 1 - math.cos(phi)
+    rho, theta = utils.R(xi, eta)
+
+    if rho <= 4:
+        if rho <= 2:
+            A = math.acos((rho + 2) / 4)
+            t = utils.M(theta + math.pi/2 + A)
+            u = utils.M(A)
+            v = utils.M(phi - t + 2*u)
+        else:
+            A = math.acos((rho - 2) / 4)
+            t = utils.M(theta + math.pi/2 - A)
+            u = utils.M(math.pi - A)
+            v = utils.M(phi - t + 2*u)
+
+        params = [t, u, u, v]
+        steerings = [Steering.LEFT, Steering.RIGHT, Steering.LEFT, Steering.RIGHT]
+        gears = [Gear.FORWARD, Gear.FORWARD, Gear.BACKWORD, Gear.BACKWORD]
+
+        for i in range(len(params)):
+            word.append(WordElement(params[i], steerings[i], gears[i]))
+
+    return word
+
+
+def word_cluster_7(x, y, phi):
+    """
+    Formule 8.8 for C|CuCu|C words
+    """
+    word = []
+
+    xi = x + math.sin(phi)
+    eta = y - 1 - math.cos(phi)
+    rho, theta = utils.R(xi, eta)
+    u1 = (20 - rho*rho) / 16
+
+    if rho <= 6 and 0 <= u1 and u1 <= 1:
+        u = math.acos(u1)
+        A = math.asin(2 * math.sin(u) / rho)
+        t = utils.M(theta + math.pi/2 + A)
+        v = utils.M(t - phi)
+
+        params = [t, u, u, v]
+        steerings = [Steering.LEFT, Steering.RIGHT, Steering.LEFT, Steering.RIGHT]
+        gears = [Gear.FORWARD, Gear.BACKWORD, Gear.BACKWORD, Gear.FORWARD]
+
+        for i in range(len(params)):
+            word.append(WordElement(params[i], steerings[i], gears[i]))
+
+    return word
+
+
+def word_cluster_8(x, y, phi):
+    """
+    Formule 8.9 for C|C[pi/2]SC words
+    """
+    word = []
+
+    xi = x - math.sin(phi)
+    eta = y - 1 + math.cos(phi)
+    rho, theta = utils.R(xi, eta)
+
+    if rho >= 2:
+        u = math.sqrt(rho*rho - 4) - 2
+        A = math.atan2(2, u+2)
+        t = utils.M(theta + math.pi/2 + A)
+        v = utils.M(t - phi + math.pi/2)
+
+        params = [t, math.pi/2, u, v]
+        steerings = [Steering.LEFT, Steering.RIGHT, Steering.STRAIGHT, Steering.LEFT]
+        gears = [Gear.FORWARD, Gear.BACKWORD, Gear.BACKWORD, Gear.BACKWORD]
+
+        for i in range(len(params)):
+            word.append(WordElement(params[i], steerings[i], gears[i]))
+
+    return word
+
+
+def word_cluster_9(x, y, phi):
+    """
+    Formule 8.9 for CSC[pi/2]|C words
+    """
+    word = []
+
+    xi = x - math.sin(phi)
+    eta = y - 1 + math.cos(phi)
+    rho, theta = utils.R(xi, eta)
+
+    if rho >= 2:
+        u = math.sqrt(rho*rho - 4) - 2
+        A = math.atan2(u+2, 2)
+        t = utils.M(theta + math.pi/2 - A)
+        v = utils.M(t - phi - math.pi/2)
+
+        params = [t, u, math.pi/2, v]
+        steerings = [Steering.LEFT, Steering.STRAIGHT, Steering.RIGHT, Steering.LEFT]
+        gears = [Gear.FORWARD, Gear.FORWARD, Gear.FORWARD, Gear.BACKWORD]
+
+        for i in range(len(params)):
+            word.append(WordElement(params[i], steerings[i], gears[i]))
+
+    return word
+
+
+def word_cluster_10(x, y, phi):
+    """
+    Formule 8.10 for C|C[pi/2]SC words
+    """
+    word = []
+
+    xi = x + math.sin(phi)
+    eta = y - 1 - math.cos(phi)
+    rho, theta = utils.R(xi, eta)
+
+    if rho >= 2:
+        t = utils.M(theta + math.pi/2)
+        u = rho - 2
+        v = utils.M(phi - t - math.pi/2)
+
+        params = [t, math.pi/2, u, v]
+        steerings = [Steering.LEFT, Steering.RIGHT, Steering.STRAIGHT, Steering.RIGHT]
+        gears = [Gear.FORWARD, Gear.BACKWORD, Gear.BACKWORD, Gear.BACKWORD]
+
+        for i in range(len(params)):
+            word.append(WordElement(params[i], steerings[i], gears[i]))
+
+    return word
+
+
+def word_cluster_11(x, y, phi):
+    """
+    Formule 8.10 for CSC[pi/2]|C words
+    """
+    word = []
+
+    xi = x + math.sin(phi)
+    eta = y - 1 - math.cos(phi)
+    rho, theta = utils.R(xi, eta)
+
+    if rho >= 2:
+        t = utils.M(theta)
+        u = rho - 2
+        v = utils.M(phi - t - math.pi/2)
+
+        params = [t, u, math.pi/2, v]
+        steerings = [Steering.RIGHT, Steering.STRAIGHT, Steering.RIGHT, Steering.LEFT]
+        gears = [Gear.FORWARD, Gear.FORWARD, Gear.FORWARD, Gear.BACKWORD]
+
+        for i in range(len(params)):
+            word.append(WordElement(params[i], steerings[i], gears[i]))
+
+    return word
+
+
+def word_cluster_12(x, y, phi):
+    """
+    Formule 8.11 for C|C[pi/2]SC[pi/2]|C words
+    """
+    word = []
+
+    xi = x + math.sin(phi)
+    eta = y - 1 - math.cos(phi)
+    rho, theta = utils.R(xi, eta)
+
+    if rho >= 4:
+        u = math.sqrt(rho*rho - 4) - 4
+        A = math.atan2(2, u+4)
+        t = utils.M(theta + math.pi/2 + A)
+        v = utils.M(t - phi)
+
+        params = [t, math.pi/2, u, math.pi/2, v]
+        steerings = [Steering.LEFT, Steering.RIGHT, Steering.STRAIGHT, Steering.LEFT, Steering.RIGHT]
+        gears = [Gear.FORWARD, Gear.BACKWORD, Gear.BACKWORD, Gear.BACKWORD, Gear.FORWARD]
+
+        for i in range(len(params)):
+            word.append(WordElement(params[i], steerings[i], gears[i]))
+
+    return word
---
+++ utils.py
@@ -0,0 +1,37 @@
+import math
+
+
+def R(x, y):
+    """
+    Polar transform (r, theta) for (x, y)
+    """
+    r = math.sqrt(x**2 + y**2)
+    theta = math.atan2(y, x)
+    return r, theta
+
+
+def M(theta):
+    """
+    Map theta to [-pi, pi)
+    """
+    theta %= 2 * math.pi
+    if theta < -math.pi: theta += 2 * math.pi
+    elif theta >= math.pi: theta -= 2 * math.pi
+    return theta
+
+
+def normalize_basis(p1, p2):
+    """
+    Normalize the basis that p1 equals (0, 0, 0)
+    """
+    dx = p2[0] - p1[0]
+    dy = p2[1] - p1[1]
+    phi1 = p1[2]
+    x_new = math.cos(phi1) * dx + math.sin(phi1) * dy
+    y_new = -math.sin(phi1) * dx + math.cos(phi1) * dy
+    phi_new = p2[2] - p1[2]
+    return x_new, y_new, phi_new
+
+
+def rad2deg(rad):
+    return rad / math.pi * 180
